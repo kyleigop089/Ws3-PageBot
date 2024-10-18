36826703a3ca43d33561484117164488937add4d
Churchill <141407524+churchillitos@users.noreply.github.com>
2024-10-18 22:32:32 +0800

    Update help.js

--- ws3/commands/help.js
+++ ws3/commands/help.js
@@ -1,40 +1,35 @@
 module.exports = {
   description: "See available commands",
   async run({ api, send, admin }) {
-  const quick_replies = [];
-  api.commands.forEach((name) => {
-    quick_replies.push({
+    const quick_replies = [];
+    api.commands.forEach((name) => {
+      quick_replies.push({
         content_type: "text",
         title: api.prefix + name,
         payload: name.toUpperCase()
+      });
     });
-  });
     try {
-    send({
-      quick_replies,
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "button",
-          text: `🤖 | These are the commands on Wie AI below.
+      send({
+        quick_replies,
+        attachment: {
+          type: "template",
+          payload: {
+            template_type: "button",
+            text: `🤖 | These are the commands on Wie AI below.
 🔎 | Click every command to see the usage.`,
-          buttons: [
-            {
-              type: "web_url",
-              url: "https://www.facebook.com/kennethfranciscoaceberos",
-              title: "Contact Admin 1"
-            },
-            {
-              type: "web_url",
-              url: "https://www.facebook.com/wieginesalpocialechavez",
-              title: "Contact Admin 2"
-            }
-         ]
+            buttons: [
+              {
+                type: "web_url",
+                url: "https://www.facebook.com/Churchill.Dev4100",
+                title: "Contact Admin"
+              }
+            ]
+          }
         }
-      }
-    });
+      });
     } catch(err){
-     return send(err.message || err);
+      return send(err.message || err);
     }
   }
 };
